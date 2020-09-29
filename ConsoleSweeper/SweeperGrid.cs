8846bfb49ec0465d7a28727164d053755896f7e3
BandersGit <andy.i.flaskan@gmail.com>
2020-09-29 11:08:51 +0200

    dubbel for loop

--- ConsoleSweeper/SweeperGrid.cs
+++ ConsoleSweeper/SweeperGrid.cs
@@ -4,13 +4,18 @@ namespace ConsoleSweeper
 {
     public class SweeperGrid
     {
-      int[,] grid = new int[16,30];
+      int[,] grid = new int[10,10];
 
       public SweeperGrid()
       {
        for (int i = 0; i < 10; i++)
        {
-        
+           
+           for (int j = 0; j < 10; i++)
+           {
+               
+           }
+
        }  
       }
     }
