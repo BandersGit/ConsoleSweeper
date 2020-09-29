c3f3fbb8391ce6fa73f2495046b1de9307278761
BandersGit <andy.i.flaskan@gmail.com>
2020-09-29 11:03:31 +0200

    Initial commit + class

---
+++ ConsoleSweeper/SweeperGrid.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleSweeper
+{
+    public class SweeperGrid
+    {
+      int[,] grid = new int[16,30];
+
+      public SweeperGrid()
+      {
+       for (int i = 0; i < 10; i++)
+       {
+        
+       }  
+      }
+    }
+}
