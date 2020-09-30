7e0a321367320583dbe90d10e853f1bed43d2907
BandersGit <andy.i.flaskan@gmail.com>
2020-09-30 11:00:28 +0200

    grid + bombs

--- ConsoleSweeper/Program.cs
+++ ConsoleSweeper/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+
 namespace ConsoleSweeper
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SweeperGrid g1 = new SweeperGrid();
+
+            for (int x = 0; x < g1.grid.GetLength(0); x++)
+            {   
+                System.Console.WriteLine();
+
+                for (int y = 0; y < g1.grid.GetLength(1); y++)
+                {
+                    Console.Write(g1.grid[x,y]);
+                    Console.Write("  ");
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- ConsoleSweeper/SweeperGrid.cs
+++ ConsoleSweeper/SweeperGrid.cs
@@ -1,22 +1,35 @@
 using System;
 
+
 namespace ConsoleSweeper
 {
     public class SweeperGrid
     {
-      int[,] grid = new int[10,10];
+        public int[,] grid = new int[10, 10];
 
-      public SweeperGrid()
-      {
-       for (int i = 0; i < 10; i++)
-       {
-           
-           for (int j = 0; j < 10; i++)
-           {
-               
-           }
+        static Random generator = new Random();
 
-       }  
-      }
+        public SweeperGrid()
+        {
+          for (int x = 0; x < grid.GetLength(0); x++)
+          {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                  grid [x,y] = 9;
+            }
+          }
+
+          for (int i = 0; i < 10; i++)
+          {
+              grid [generator.Next(0,10), generator.Next(0,10)] = 10;
+          }
+
+        }
+
+        public bool CheckPosition(int x, int y)
+        {
+          return true;
+        }
+
     }
 }
